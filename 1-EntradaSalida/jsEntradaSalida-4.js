09b4e126960c5bb4d74ef22f8150763ff7b3dd65
massimokris <47436653+massimokris@users.noreply.github.com>
2019-02-07 22:06:09 -0300

    other commit
    exercise 4

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -7,8 +7,10 @@ function mostrar()
 	//
     var name; 
 	name = prompt("Your name is: ");
-	//Hay dos formas de invocar(la comentada es una, la aplicada es otra)
+	//Hay dos formas de invocar
+	//(la comentada es una, la aplicada es otra)
 	// nombre=elNombre.value;
     document.getElementById('elNombre').value=name;
+    // voy a agregar un alert que no pide el ejercicio pero lo quiero agregar
+    alert(name);
 }
-

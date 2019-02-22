b364665227e8eda6b05e4b295d0422170cc93f4a
alumno <alumno>
2019-02-21 21:19:23 -0300

    clase a la mitad

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,53 @@
 function mostrar()
 {
+	var habitacion;
+	var precioHabitacion;
+	var promociones;
+	var tipoDePago;
+	var tipoDePaquete;
+	var subtotal;
+	var total;
+	var dias;
+
+	promociones=100;
+
+	habitacion=prompt("Ingrese la habitacion: ");
+	precioHabitacion=prompt("Precio de la habitacion: ");
+	dias=prompt("Cuantos dias se quedara: ");
+	tipoDePago=prompt("Tipo de pago: ");
+	tipoDePaquete=prompt("Tipo de Paquete: ");
+
+	switch(tipoDePago){
+
+		case"visa":
+		case"mercadoPago"
+		promociones=90;
+		break;
+		case"paypal":
+		promociones=85;
+		case"efectivo":
+		promociones=80
+		break;
+		default:
+		promociones=95;
+	}
+
+	subtotal=precioHabitacion*dias;
+	total=subtotal*promociones/100;
+	alert("Su habitacion es: "+habitacion+" El precio por dia es: "+precioHabitacion+" el precio subtotal es: "+subtotal+" Usted pagara: "+total+" y tendra el paquete "+tipoDePaquete);
 
 }
+
+//switch(tipoDePaquete){
+			//ase"todoIncluido":
+			//promociones=75;
+		//}
+		//break;
+		//switch(tipoDePaquete){
+			//case"soloDesayunos":
+			//promociones=70;
+			//break;
+			//case"todoIncluido":
+			//promociones=65;
+			//break;
+		//}
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,23 +1,86 @@
 function mostrar()
 {
-	var amigos;
-	var conPropina;
-	var pagoIndividual;
+
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var precioCuatro;
+	var precioMayor;
+	var descuento;
+	var subtotal;
+	var mensaje;
 	var total;
-	var pagoTotal;
 
-	total=prompt("Ingrese el total a pagar: ");
-	amigos=prompt("Ingrese la cantidad de amigos: ")
+	precioUno=prompt("Ingrese el primer producto: ");
+	precioDos=prompt("Ingrese el primer producto: ");
+	precioTres=prompt("Ingrese el primer producto: ");
+	precioCuatro=prompt("Ingrese el primer producto: ");
 
-	var conPropina=parseInt(conPropina);
-	var total=parseInt(total);
+	var precioUno=parseInt(precioUno);
+	var precioDos=parseInt(precioDos);
+	var precioTres=parseInt(precioTres);
+	var precioCuatro=parseInt(precioCuatro);
+
+	subtotal=precioUno+precioDos+precioTres+precioCuatro;
+
+	descuento=100;
+
+	if(subtotal>100){
+
+		descuento=90;
+	}else{
+
+		if(subtotal<50){
+
+			descueto=115;
+		}else{
+
+			descuento=95;
+		}
+	}
+
+	total=subtotal*descuento/100;
+
+	if(precioUno>precioDos && precioUno>precioTres && precioUno>precioCuatro){
+
+		mensaje=("El precio mayor es "+precioUno+" y el total es: "+total);
+	}else{
+
+		if(precioDos>precioUno && precioDos>precioTres && precioDos>precioCuatro){
+
+			mensaje=("El precio mayor es "+precioDos+" y el total es: "+total);
+		}else{
+
+			if(precioTres>precioUno && precioTres>precioTres && precioTres>precioCuatro){
+
+				mensaje=("El precio mayor es "+precioTres+" y el total es: "+total);
+			}else{
+
+				mensaje=("El precio mayor es "+precioCuatro+" y el total es: "+total);
+			}
+		}
+	}
+
+	alert(mensaje);
+
+	
+	//var amigos;
+	//var conPropina;
+	//var pagoIndividual;
+	//var total;
+	//var pagoTotal;
+
+	//total=prompt("Ingrese el total a pagar: ");
+	//amigos=prompt("Ingrese la cantidad de amigos: ")
+
+	//var conPropina=parseInt(conPropina);
+	//var total=parseInt(total);
     
-    conPropina=total*10/100;
-    pagoTotal=conPropina+total;
-    pagoIndividual=pagoTotal/amigos;
+    //conPropina=total*10/100;
+    //pagoTotal=conPropina+total;
+    //pagoIndividual=pagoTotal/amigos;
 
-    alert(+amigos+" amigos comieron y gastaron "+total+" dieron de propina "+conPropina+" en total gastaron "+pagoTotal+" y cada uno pago "+pagoIndividual)
-
-
+    //alert(+amigos+" amigos comieron y gastaron "+total+" dieron de propina "+conPropina+" en total gastaron "+pagoTotal+" y cada uno pago "+pagoIndividual)
 
 }
+

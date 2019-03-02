73706d04fe0b395dcfe548e3fb89ed95b4d8100e
alumno <alumno@utnfra.com.ar>
2019-03-01 22:11:35 -0300

    practica parcial 9 hacer en casa

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,12 @@
 function mostrar()
 {
-
+//Realizar el algoritmo que permita ingresar la marca de la marca de la gaseosa,la
+//cantidad de litros (validar entre 1 litro, 2 litros y 3 litros), el precio el cual debe ser
+//entre 30 y 100 y
+//por document.write:
+//a)La cantidad de precios pares.
+//b)La marca y litros del más barato
+//c)La cantidad de gaseosas que valen menos de 50.
+//d)El promedio del precio de todas las gaseosa.
+//f)El precio máximo y el mínimo de todas las gaseosa de menores de 3 litros
 }
